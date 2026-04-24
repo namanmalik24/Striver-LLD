10fe3a653f36e4de26a56133f15f4b4fda10b56d
namanmalik24 <naman.m888@gmail.com>
2026-04-24 13:30:24 +0000

    YAGNI

---
+++ design-principals/YAGNI/yagni.java
@@ -0,0 +1,9 @@
+package design-principals.YAGNI;
+
+public class yagni {
+    public static void main(String[] args) {
+        // you aren't gonna need it
+        // do not add functionality until you actually need it
+    }
+    
+}
