6a3ffceecb30c953170d6904e18b484512550d6f
namanmalik24 <naman.m888@gmail.com>
2026-04-25 22:23:49 +0000

    single response principal

---
+++ design-principals/S-single-responsibility/sPrincipal.java
@@ -0,0 +1,37 @@
+class sPrincipal{
+    // one class should have only one principal
+
+ 
+
+
+}
+
+class Item{
+       int price;
+       String name;
+}
+
+class ShoppingCart{
+    void calculatePrice(){
+
+    }
+
+    void printInvoice(){
+
+    }
+
+    void saveToDatabase(){
+
+    }
+
+    // this class has more than one responsibility
+
+}
+
+//better way to do it is:
+
+// class shoppingCart should only print price
+// db should only save to database
+// invoice should only print invoice
+
+// both db and invoice should use shoppingCart to get price and other details
