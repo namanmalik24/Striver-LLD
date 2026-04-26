1712017d8a178c44e2b9809a5d8ef402cd6fc830
namanmalik24 <naman.m888@gmail.com>
2026-04-26 10:14:53 +0000

    intro to design patterns

---
+++ creationalDesignPatterns/intro/Intro.java
@@ -0,0 +1,12 @@
+package creationalDesignPatterns.intro;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    //creational
+    //structural
+    //behavioral
+    // these are the 3 types of design patterns
+}
