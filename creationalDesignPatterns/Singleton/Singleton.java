cd575b20fe2d1d4a4b13b1b56b3557b692fc6a2d
namanmalik24 <naman.m888@gmail.com>
2026-05-04 18:36:45 +0000

    added theory for singleton

--- creationalDesignPatterns/Singleton/Singleton.java
+++ creationalDesignPatterns/Singleton/Singleton.java
@@ -1,33 +1,32 @@
 package creationalDesignPatterns.Singleton;
 
 class JudgeAnalytics{
-    private int run;
-    private int submit;
+    private static final JudgeAnalytics judgeAnalytics = new JudgeAnalytics();
 
-    public void countRun(){
-        run++;
+    private JudgeAnalytics() {
     }
-    public void getCountRun(){
-        run++;
+    public static JudgeAnalytics getInstance(){
+        return judgeAnalytics;
     }
-    public int getSubmitCount(){
-        return submit;
-    }   
+       
 }
 
 public class Singleton {
     public static void main(String[] args) {
 
-        JudgeAnalytics judgeAnalytics = new JudgeAnalytics();
-        judgeAnalytics.countRun();
-        judgeAnalytics.getSubmitCount();
+        JudgeAnalytics judgeAnalytics = JudgeAnalytics.getInstance();
 
-        JudgeAnalytics judgeAnalytics2 = new JudgeAnalytics();
-        // if someone else is using it, then new object created
-        //this object will be diff
-        
+     
 
+// How do you force a class to be a Singleton?
 
+// You need three bouncers at the door:
+
+// A Private Constructor: This hides the "creation button" (new keyword). Nobody outside the class is allowed to create an object from it.
+
+// A Static Variable: This is a special display case inside the class where the single, unique object is kept.
+
+// A Public Static Method (usually called getInstance()): This is the window where people can ask for the object. Every time someone asks, this method hands them the exact same object from the display case.
 
 
 
