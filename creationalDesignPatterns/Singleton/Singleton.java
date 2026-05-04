5b23b893f121f15eda0e8dfe652b55c1b6f4c23a
namanmalik24 <naman.m888@gmail.com>
2026-05-04 18:55:18 +0000

    added 3 methods to create object in singleton pattern

--- creationalDesignPatterns/Singleton/Singleton.java
+++ creationalDesignPatterns/Singleton/Singleton.java
@@ -1,5 +1,7 @@
 package creationalDesignPatterns.Singleton;
 
+import java.lang.reflect.Method;
+
 class JudgeAnalytics{
     private static final JudgeAnalytics judgeAnalytics = new JudgeAnalytics();
 
@@ -28,8 +30,58 @@ public class Singleton {
 
 // A Public Static Method (usually called getInstance()): This is the window where people can ask for the object. Every time someone asks, this method hands them the exact same object from the display case.
 
+// Method A: Eager Loading (Build it immediately)
+// Method B: Lazy Loading (Build it only when asked)
 
 
 
     }
 }
+
+
+class EagerSingleton {
+    // 1. Create the object IMMEDIATELY
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    // 2. Lock the door
+    private EagerSingleton() {
+    }
+
+    // 3. Hand it out
+    public static EagerSingleton getInstance() {
+        return instance; 
+    }
+}
+//1. Eager Loading (The "Fire Extinguisher" Approach)
+
+class LazySingleton {
+    // 1. Start with an empty box
+    private static LazySingleton instance;
+
+    // 2. Lock the door
+    private LazySingleton() {
+    }
+
+    // 3. Build it ONLY if asked
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton(); // Build it!
+        }
+        return instance;
+    }
+}
+//2. Lazy Loading (The "Coffee Machine" Approach)
+
+public class Singleton {
+    private Singleton() {}
+
+    // A hidden inner class
+    private static class Holder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return Holder.INSTANCE;
+    }
+}
+//3. The Best Practice: Bill Pugh Singleton
